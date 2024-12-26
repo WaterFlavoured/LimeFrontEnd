d64ee36975a7e5d73bdad1ddada9432b8287f26e
WaterFlavoured <michaelz9478@gmail.com>
2024-12-26 11:24:57 -0500

    Testing :)

--- limestonefrontend/src/App.jsx
+++ limestonefrontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes, Route } from "react-router";
+import Home from './pages/Home'
+import About from './pages/About'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   )
 }
---
+++ limestonefrontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import video1 from '../assets/italy.mp4';
+import './Home.css'
+const Home = () => {
+  return (
+    <div className="main">
+      <video className="background-video" src={video1} autoPlay loop muted />
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
