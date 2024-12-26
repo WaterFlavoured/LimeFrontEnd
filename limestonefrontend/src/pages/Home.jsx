35bd2581f746af0443cfc27ce38b66213a55ab04
WaterFlavoured <michaelz9478@gmail.com>
2024-12-26 13:07:10 -0500

    Front page setup, video, styling

--- limestonefrontend/src/pages/Home.jsx
+++ limestonefrontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import video1 from '../assets/italy.mp4';
+import logo from '../assets/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquarePlus } from '@fortawesome/free-regular-svg-icons'
 import './Home.css'
 const Home = () => {
   return (
     <div className="main">
       <video className="background-video" src={video1} autoPlay loop muted />
-      <div>
-        
+      <div className='content'>
+        <h1><img src={logo} alt="" /></h1>
+        <button className='plus'><FontAwesomeIcon icon={faSquarePlus} /></button>
       </div>
     </div>
   );
