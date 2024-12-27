f416dc055082c1d78a9cd5e26c2b0d4287f8389c
WaterFlavoured <michaelz9478@gmail.com>
2024-12-26 22:42:12 -0500

    Mostly complete nav bar

--- limestonefrontend/src/components/SideBar.jsx
+++ limestonefrontend/src/components/SideBar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './SideBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router';
 const SideBar = ({ setShowNav }) => {
   return (
@@ -11,12 +13,19 @@ const SideBar = ({ setShowNav }) => {
           <FontAwesomeIcon icon={faBars} />
         </button>
         <ul>
-          <li><Link to="/">Home</Link></li>
           <li><Link to="/pages/about">About</Link></li>
           <li><Link to="/pages/portfolio">Portfolio</Link></li>
           <li><Link to="/pages/rentals">Rentals</Link></li>
-          <li><Link to="/pages/contact">Contact</Link></li>
+          <li><Link to="/pages/contact">Contact Us</Link></li>
         </ul>
+        <div className='sfooter'>
+          <div className='social'>
+            <a href="https://www.instagram.com/limestone_concrete?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><FontAwesomeIcon icon={faInstagram}/></a>
+            <a href="https://www.tiktok.com/@limestone_concreteltd?is_from_webapp=1&sender_device=pc"><FontAwesomeIcon icon={faTiktok} /></a>
+          </div>
+          <p>© LIMESTONE CONCRETE LTD.</p>
+          <p>DESIGNED BY LIMESTONE CONCRETE LTD</p>
+        </div>
       </div>
     </div>
   );
