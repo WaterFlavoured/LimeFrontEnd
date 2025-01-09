86c7c083163d5b1d9e1c2e1303e98b9e6e5ba624
karim.m870 <kmohama2@uwo.ca>
2025-01-09 17:14:51 -0500

    Started rental page

--- limestonefrontend/src/pages/Rentals.jsx
+++ limestonefrontend/src/pages/Rentals.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './Rentals.css'
+import image1 from '../assets/RentalsPhotos/ConcreteStamp.jpg'
+import image2 from '../assets/RentalsPhotos/WireMesh.jpg'
+
+
 const Rentals = () => {
   return (
-    <div>Rentals</div>
+    <div className='rentals'>
+
+      <h1 style={{ fontSize: '45px' }} className='header'>RENTALS</h1>
+
+      <div className='main-body'>
+
+        <div className='leftSide'>
+          <img className='imgs' src={image1}></img>
+          <p>This concrete stamp does a lot of stuff but idrk what</p>
+        </div>
+
+        <div className='rightSide'>
+          <img className='imgs' src={image2}></img>
+          <p>This is some wire mesh and it also does stuff but again idk what it is</p>
+
+        </div>
+
+
+      </div>
+
+
+
+    </div>
   )
 }
 
